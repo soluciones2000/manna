b5d4ebfd00ccd0f8233cdd1db2737aac674829cc
Luis Rodríguez <soluciones2000@gmail.com>
2018-01-26 17:27:55 -0400

    Correccion parametros

--- admin/parametros.php
+++ admin/parametros.php
@@ -7,7 +7,8 @@ include_once("menu.php");
 $men2 = "parametros";
 include_once("opciones.php");
 //	$query = "select * from information_schema.columns where table_schema='corpmann_manna' and table_name='afiliados'";
-	$query = "select * from information_schema.columns where table_schema='manna' and table_name='empresa'";	
+	$query = "select * from information_schema.columns where table_schema='sgcconsu_manna' and table_name='empresa'";	
+//	$query = "select * from information_schema.columns where table_schema='manna' and table_name='empresa'";	
 	$result = mysql_query($query,$link);
 
 
